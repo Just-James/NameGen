8443d99aad4f23480170c387acd63bdc00b1ae2a
Just-James <Jacku.simon@gmail.com>
2015-06-08 23:00:56 +0100

    10 names 1 random 2 ints 2 souts  basic functionality

--- src/Prac/NameGen/Main.java
+++ src/Prac/NameGen/Main.java
@@ -1,8 +1,37 @@
 package Prac.NameGen;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello");
+        List<String> firstNames = new ArrayList<>();
+        firstNames.add("Bob");
+        firstNames.add("Bill");
+        firstNames.add("Ben");
+        firstNames.add("Beth");
+        firstNames.add("Beatrix");
+        List<String> surNames = new ArrayList<>();
+        surNames.add("Bullworth");
+        surNames.add("BeanShallow");
+        surNames.add("Bagton");
+        surNames.add("Batmyth");
+        surNames.add("Byson");
+
+        Random randomgen = new Random();
+
+        int rand1 = randomgen.nextInt(4);
+        int rand2 = randomgen.nextInt(4);
+
+        System.out.println(firstNames.get(rand1));
+        System.out.println(surNames.get(rand2));
+
+
+
+
+
+
     }
 }
